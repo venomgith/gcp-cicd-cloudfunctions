a8d356d088c0dc58cb1906aaab4b47a45877ba86
venomgith <97960831+venomgith@users.noreply.github.com>
2023-03-06 13:28:49 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+//-----------------------------------------------------------------------------
+// NodeJS App for GCP Cloud Functions deployed via GCP Cloud Build Triggers
+// Copyleft (c) by Denis Astahov
+//-----------------------------------------------------------------------------
+
+exports.helloWorld = (req, res) => {
+  const message="<font color='blue'>СloudFunction of Ellington!</font><br><b>App Version 1.2</b>";
+  res.status(200).send(message);
+};
